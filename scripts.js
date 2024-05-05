8c75063155bf3f5cd9a41e80d6241423ce4c0e50
hosseingh75 <hossein.gharibzade@yahoo.com>
2024-05-05 09:29:59 +0330

    create project

---
+++ scripts.js
@@ -0,0 +1,177 @@
+var count = 0;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; // حد بالا (max) نیز در نظر گرفته می‌شود
+}
+
+function btn1() {
+    const btn = document.getElementById('btn1');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn2() {
+    const btn = document.getElementById('btn2');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn3() {
+    const btn = document.getElementById('btn3');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn4() {
+    const btn = document.getElementById('btn4');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn5() {
+    const btn = document.getElementById('btn5');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn6() {
+    const btn = document.getElementById('btn6');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn7() {
+    const btn = document.getElementById('btn7');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn8() {
+    const btn = document.getElementById('btn8');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn9() {
+    const btn = document.getElementById('btn9');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
+
+function btn10() {
+    const btn = document.getElementById('btn10');
+
+    btn.style.position = 'absolute';
+
+    const rand1 = getRandomInt(1,90);
+    const rand2 = getRandomInt(1,90);
+
+    btn.style.top = `${rand1}%`;
+    btn.style.left = `${rand2}%`;
+
+    count++;
+
+    // if (count == 5)
+    //     alert('باشه دیگه بخشیدمت');
+}
